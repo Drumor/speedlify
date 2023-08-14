ff341a556f0924453428989b102cf3f83f8bb8ef
Ludovic Taffin <ludovic.taffin@lolaliza.com>
2023-08-14 10:55:55 +0200

    first test for ll.com

---
+++ _data/sites/test-one.js
@@ -0,0 +1,10 @@
+module.exports = {
+    description: "Lolaliza.com",
+    options: {
+        runs: 1,
+		frequency: 1, // (in minutes)
+    },
+    urls: [
+        "https://www.lolaliza.com/",
+    ]
+};
