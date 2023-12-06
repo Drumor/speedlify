db385a2de0f0e46118d8ea335e1e6d3b4e428703
Ludovic Taffin <Drumor@users.noreply.github.com>
2023-12-06 08:35:33 +0100

    Update test-one.js

--- _data/sites/test-one.js
+++ _data/sites/test-one.js
@@ -5,11 +5,11 @@ module.exports = {
 		frequency: 1, // (in minutes)
     },
     urls: [
-        "https://www.lolaliza.com/",
+        "https://www.lolaliza.com/be-nl/home",
 	"https://development.lolaliza.com",
 	"https://development.lolaliza.com/be-nl/kleding/jurken/1101",
-	"https://www.lolaliza.com/be-en/clothing/dresses/",
+	"https://www.lolaliza.com/be-nl/clothing/dresses/",
 	"https://development.lolaliza.com/be-nl/wikkeljurk_met_ceintuur-red_lollipop-08103199_5301.html",
-	"https://www.lolaliza.com/be-en/cable-knit_pullover_dress-camel-08103304_950.html",
+	"https://www.lolaliza.com/be-nl/cable-knit_pullover_dress-camel-08103304_950.html",
     ]
 };
