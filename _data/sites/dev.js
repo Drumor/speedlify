d04825a10343f1e8622ad881cecca570813cc296
Ludovic Taffin <Drumor@users.noreply.github.com>
2023-12-06 09:03:59 +0100

    Create dev.js

---
+++ _data/sites/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    description: "development.lolaliza.com",
+    options: {
+        runs: 1,
+		frequency: 1, // (in minutes)
+    },
+    urls: [
+      "https://development.lolaliza.com",
+      "https://development.lolaliza.com/be-nl/kleding/jurken/1101",
+      "https://development.lolaliza.com/be-nl/wikkeljurk_met_ceintuur-red_lollipop-08103199_5301.html",
+    ]
+};
