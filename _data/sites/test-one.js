7fb54c7e1e8a52556a07ba4c01c7479fa5d203f6
Ludovic Taffin <Drumor@users.noreply.github.com>
2023-12-06 09:15:45 +0100

    Update test-one.js

--- _data/sites/test-one.js
+++ _data/sites/test-one.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     urls: [
         "https://www.lolaliza.com/be-nl/home",
-	"https://www.lolaliza.com/be-nl/clothing/dresses/",
+	"https://www.lolaliza.com/be-nl/kleding/jurken/",
 	"https://www.lolaliza.com/be-nl/kabelgebreide_trui-jurk-camel-08103304_950.html",
     ]
 };
